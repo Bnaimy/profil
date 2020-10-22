2374fc3459977979bc3d38229531f8f21ffd47e8
NAIMYS <naimy.soukaina@gmail.com>
2020-10-22 14:45:42 +0100

    cc

---
+++ src/components/about.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Us</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I am a computer science student from EMSI called chouaib . I have passion with IT and all linked with make the world great of course with science technologie . information is expensive ! so you need to pay to get it however the payment method !</p>
+                    <p>I have power to help people , grow up my skills . Im bouchaib naimy and dev is my life ! </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I Can do?</span>
+                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>I have experience building web apps using React.js,Spring boot and tailwind</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Data Structures & Algorithms</h3>
+                    <p>I have a good background about neural networks , python algos for data science </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>DATABASES</h3>
+                    <p>I am use frequently mysql database , but also i have powerful vocabulary in no-relationnal databases (NO SQL) like mongoDB</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>BAC SCIENCE MATH OPTION A <span>2016-2017</span></h2>
+                        <p>I took my bac diploma in 2017 , it was a great experience to study in hard section like math but i was also great ;) .</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>DUT COMPUTER SCIENCE DIPLOMA <span>2017-2019</span></h2>
+                        <p>before my bac diploma i have been a passion to computer science and latest technologies for that I took in my goals apply for EST SCHOOL section computer science . </p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>STUDENT ENGINNER IN EMSI SCHOOL <span>2019-present</span></h2>
+                        <p>I recently have joined emsi school to continue my studies , i'm now in the second year and stay only one year to achieve my principal goal -> enginner in computer science specificly cloud architect :)</p>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
